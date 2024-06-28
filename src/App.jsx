89f16856b223a963a0d40ceaff920a6cb9c88a1b
jas112 <jas.sandmaster@gmail.com>
2024-06-28 02:40:11 -0700

    modifications to app.jsx - state play

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
+import './App.css'
 
 const App = () => {
-  
+
+  const myName = 'SpaceTRIPP';
+  const names = ['thoth', 'bast', 'khonshu', 'osirus'];
+  // const isActive = true;
+  const [isActive, setIsActive] = useState(true);
+
+  const handleButtonPress = () => {
+    setIsActive(isActive => !isActive);
+  };
+
   return (
-    <div>App</div>
+    <div>
+      <div className='init-text'>{myName}</div>
+      <ul>
+        {names.map((nValue,index) => (
+          <li>{nValue}</li>
+        ))}
+      </ul>
+      <button onClick={handleButtonPress}>
+          isActive - {isActive.toString()}
+      </button>
+      { isActive ? <h1 className='isActiveText'>It is active in here!!!</h1> : <h1 className='isNotActiveText'>It is not active in here!!</h1> }
+    </div>
   )
 }
 
