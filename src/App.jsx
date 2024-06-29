07b564c0af9e79f248caca1087b2a26ca0c6e6a1
jas112 <jas.sandmaster@gmail.com>
2024-06-29 00:43:31 -0700

    modifications to mainContent component folder, navbar component modifications (props - time and scroll), app.jsx modification (time, date, scroll tracking)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import NavBar from './Components/navBar/NavBar';
 import ForegroundFrame from './Components/parallaxFrames/ForegroundFrame.jsx';
@@ -6,18 +6,79 @@ import BackgroundFrame from './Components/parallaxFrames/BackgroundFrame.jsx';
 
 const App = () => {
 
-  const myName = 'SpaceTRIPP';
-  const names = ['thoth', 'bast', 'khonshu', 'osirus'];
-  // const isActive = true;
-  const [isActive, setIsActive] = useState(true);
+  const [scrollPositionY, setScrollPositionY] = useState(0);
+  const [timeString, setTimeString] = useState('');
 
-  const handleButtonPress = () => {
-    setIsActive(isActive => !isActive);
-  };
+  useEffect(() => {
+
+    const trackScroll = () => {
+      const newScrollPositionValue = window.scrollY;
+      setScrollPositionY(newScrollPositionValue);
+    };
+
+    window.addEventListener('scroll', trackScroll);
+
+    const trackTime = setInterval(() => {
+
+      var dateValue = new Date();
+      var yearValue = dateValue.getFullYear();
+      var monthValue = dateValue.getMonth();
+      var dayValue = dateValue.getDate();
+      var hourValue = dateValue.getHours()
+      var minutesValue = dateValue.getMinutes();
+      var secondsValue = dateValue.getSeconds();
+      var months = ["JAN. ", "FEB. ", "MAR. ", "APR. ", "MAY. ", "JUN. ", "JUL. ", "AUG. ", "SEP. ", "OCT. ", "NOV. ", "DEC. "];
+      var days = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+      var mdvValue = months[monthValue] + dayValue;
+      var yrValue = yearValue;
+
+      var hoursStr;
+
+      if(hourValue < 10){
+        hoursStr = "0" + hourValue;
+      }else{
+        hoursStr = hourValue;
+      }
+
+      var minutesStr;
+
+      if(minutesValue < 10){
+        minutesStr = "0" + minutesValue;
+      }else{
+        minutesStr = minutesValue;
+      }
+
+      var secondsStr;
+
+      if(secondsValue < 10){
+        secondsStr = "0" + secondsValue;
+      }else{
+        secondsStr = secondsValue;
+      }
+
+      var tmStrValue = mdvValue + ', ' + yrValue + ' | ' + hoursStr + "hrs : " + minutesStr + "min : " + secondsStr + 'sec';
+
+      setTimeString(tmStrValue)
+
+    }, 1000)
+
+    return () => {
+      window.removeEventListener('scroll', trackScroll);
+      clearInterval(trackTime);
+    };
+  }, [])
+
+  // const myName = 'SpaceTRIPP';
+  // const names = ['thoth', 'bast', 'khonshu', 'osirus'];
+  // const [isActive, setIsActive] = useState(true);
+
+  // const handleButtonPress = () => {
+  //   setIsActive(isActive => !isActive);
+  // };
 
   return (
     <>
-      <NavBar/>
+      <NavBar currentScroll={scrollPositionY} currentTime={timeString} />
       <ForegroundFrame/>
       <BackgroundFrame/>
       {/* <div>
--- src/Components/navBar/NavBar.jsx
+++ src/Components/navBar/NavBar.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './styles/NavBar.css';
 import stLogo from '../../assets/images/siteFeatures_imgs/ST_logo-RSCCsm.png'
 
-const NavBar = () => {
+const NavBar = ({currentScroll, currentTime}) => {
+
   return (
     <nav className='navbar'>
         <img src={stLogo} className='navbar-logo' alt='SpaceTripp Logo'/>
         <div className='navbar-links'>
+            SROLV-{currentScroll} {currentTime}
             <a href='#start' className='navbar-link'>&#10094;&#10094; &#9737;</a>
             <a href='#about' className='navbar-link'>About</a>
             <a href='#webDev' className='navbar-link'>Web+Dev</a>
